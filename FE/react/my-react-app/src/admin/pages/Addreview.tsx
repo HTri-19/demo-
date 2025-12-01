2309adb8da4a66c4e6920bb2c101feb143f92862
HTri-19 <htri1907@gmail.com>
2025-12-01 21:53:24 +0700

    update

--- FE/react/my-react-app/src/admin/pages/Addreview.tsx
+++
@@ -1,7 +0,0 @@
-// import { Link } from "react-router-dom";
-import "../assets/css/AddReview.css";
-const Addreview = () => {
-  return <></>;
-};
-
-export default Addreview;
--- README.md
+++
@@ -1,2 +0,0 @@
-# demo-
-khong rot mon
